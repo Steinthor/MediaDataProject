fc518fec23f4628c54d5611409ebb34fc912c1ca
rjoe5020 <rjoechl.its-b2014@fh-salzburg.ac.at>
2018-04-27 17:23:38 +0200

    surf demo

--- SURF_demo.py
+++ SURF_demo.py
@@ -1,6 +1,8 @@
 import imageio
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial import distance
 
 #blabla
 image_name = "001_F"
@@ -9,11 +11,29 @@ image_ending = ".png"
 load_path = "data/CoMoFoD/"
 img_test = imageio.imread(load_path+image_name+image_ending, format="PNG-FI")
 
-surf = cv2.xfeatures2d.SURF_create()
+surf = cv2.xfeatures2d.SIFT_create()
 (kps, descs) = surf.detectAndCompute(img_test, None)
 img_surf = cv2.drawKeypoints(img_test, kps, None, color=(0, 255, 0), flags=0)
 
+dist = np.empty([descs.shape[0],2])
+print(descs.shape)
+for i in np.arange(descs.shape[0]):
+    for j in np.arange(descs.shape[0]):
+        dist[j][0] = distance.euclidean(descs[i],descs[j])
+        dist[j][1] = j
 
+    dist = dist[np.argsort(dist[:, 0])]
+    print(dist)
+
+    k = 0
+    q = 0
+    while q < 0.6:
+        k = k + 1
+        q = dist[k]/dist[k+1]
+
+
+
+#dst = distance.euclidean(a,b)
 
 fig, axes = plt.subplots(1, 2)
 axes[0].imshow(img_test)
